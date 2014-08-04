9788ba4aa799b56bbc29a9412e26efb1c10c458a
jblossom <jblossom@cinemaequip.com>
2014-08-04 14:53:03 -0400

    preserve legacy app, init codeigniter

---
+++ legacy/config/database.php
@@ -0,0 +1,14 @@
+
+; This is an ini file
+; http://php.net/manual/en/function.parse-ini-file.php
+
+; This is the Config for the Database connection details
+; doing it this way, lets the configs be ignored by git, 
+; meaning, each user can have separate configs by copying database.sample.php
+; and modifying it accordingly
+
+[DB]
+host = localhost
+user = arxtest
+pass = mLt8gF@7Hq8kA
+database = arx_test
